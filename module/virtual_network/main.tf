eee634456aa89d86e65df46e04b92555aed39d29
yami2254 <yami2254@naver.com>
2023-02-20 14:28:47 +0900

    virtual network ver 1.0 add

--- module/virtual_network/main.tf
+++ module/virtual_network/main.tf
@@ -1,36 +1,18 @@
-data "azurerm_resource_group" "import-rg" {
-  count = var.is_portal_vnet == false ? 0 : 1
-  name = var.import_resource_group_name
-}
-
-data "azurerm_virtual_network" "vnet" {
-  count = var.is_portal_vnet == false ? 0 : 1
-  name = var.import_vnet
-  resource_group_name = data.azurerm_resource_group.import-rg[count.index].name
-}
-
-
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
-
-
 resource "azurerm_virtual_network" "vnet" {
-  count = var.is_portal_vnet == false ? 1 : 0
   name                = var.vnet_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
   address_space       =  length(var.address_spaces) == 0 ? var.address_space : [0]
   tags                = var.tags
 }
-
-
 resource "azurerm_subnet" "subnet" {
-  count                                          = var.is_portal_subnet == false ? length(var.subnet_name) : 0 
+  count                                          = length(var.subnet_name)
   name                                           = var.subnet_name[count.index]
-  resource_group_name                            = var.resource_group_name
+  resource_group_name                            = data.azurerm_resource_group.rg.name
   address_prefixes                               = [var.subnet_prefixes[count.index]]
   virtual_network_name                           = var.vnet_name
-
   depends_on  = [azurerm_virtual_network.vnet]
 }
--- resource/virtual_network/variable.tfvars
+++ resource/virtual_network/variable.tfvars
@@ -1,25 +1,11 @@
-#----Azure DevOps Variable에서 처리되는 부분으로 주석
-# resource_group_name = "ski-cims-dev-rg"
-# vnet_name = "ski-cims-vnet-01"
-# cidr = ["10.4.0.0/16"]
-# subnet = ["10.4.1.0/24","10.4.2.0/24"]
-# subnet_name = ["ski-cims-dev-ap-subnet", "ski-cims-dev-db-subnet"]
-
-# tags = {
-#    cz-corp         = "SK이노베이션"
-#    cz-oc           = "SK이노베이션"
-#    cz-project      = "전화정보시스템"
-#    cz-org          = "Hybrid Cloud2그룹"
-#    cz-owner        = "XXX"
-#    cz-appl         = "Network"
-#    cz-stage        = "개발"
-#    cz-created      = "2022-05-XX"
-#    cz-createdby    = "XXX"
-# }
-
-#-----"수정하지 말 것"
-is_portal_vnet = false
-is_portal_subnet = false
-import_vnet = "import-vnet"
-import_resource_group_name = "import-rg"
+#예시
+resource_group_name = "testrg"
+vnet_name = "az-test-vnet-001"
+cidr = ["10.4.0.0/16"]
+subnet = ["10.4.1.0/24","10.4.2.0/24"]
+subnet_name = ["az-test-dev-ap-subnet", "az-test-dev-db-subnet"]
+tags = {
+    key1 = "Value3"
+    key2 = "Value2"
+}
 location            = "koreacentral"
