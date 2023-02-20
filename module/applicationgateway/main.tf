152dc34cb7443e2467a54720270091612293a072
bonah koo <guavaguava0618@gmail.com>
2023-02-20 11:18:38 +0900

    WAF : Standard 조건부 설정

--- module/applicationgateway/main.tf
+++ module/applicationgateway/main.tf
@@ -22,12 +22,6 @@ data "azurerm_subnet" "frontend" {
   virtual_network_name = data.azurerm_virtual_network.example.name
 }
 
-data "azurerm_subnet" "backend" {
-  name                 = var.appgw_subnet_name2
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = data.azurerm_virtual_network.example.name
-}
-
 resource "azurerm_public_ip" "example" {
   name                = var.public_ip.name
   resource_group_name = data.azurerm_resource_group.example.name
@@ -36,18 +30,10 @@ resource "azurerm_public_ip" "example" {
   sku                 = var.public_ip.sku
 }
 
-# since these variables are re-used - a locals block makes this more maintainable
-locals {
-  backend_address_pool_name      = "${data.azurerm_virtual_network.example.name}-beap"
-  frontend_port_name             = "${data.azurerm_virtual_network.example.name}-feport"
-  frontend_ip_configuration_name = "${data.azurerm_virtual_network.example.name}-feip"
-  http_setting_name              = "${data.azurerm_virtual_network.example.name}-be-htst"
-  listener_name                  = "${data.azurerm_virtual_network.example.name}-httplstn"
-  request_routing_rule_name      = "${data.azurerm_virtual_network.example.name}-rqrt"
-  redirect_configuration_name    = "${data.azurerm_virtual_network.example.name}-rdrcfg"
-}
+
 
 resource "azurerm_web_application_firewall_policy" "network" {
+  count = var.isWAF ? 1:0  
   name                = var.web_policy.name
   resource_group_name = data.azurerm_resource_group.example.name
   location            = data.azurerm_resource_group.example.location
@@ -59,17 +45,16 @@ resource "azurerm_web_application_firewall_policy" "network" {
 }
 
 resource "azurerm_application_gateway" "network" {
-  name                = var.appgw
+  name                = var.appgw_config.name
   resource_group_name = data.azurerm_resource_group.example.name
   location            = data.azurerm_resource_group.example.location
 
-  firewall_policy_id = azurerm_web_application_firewall_policy.network.id
-
+  firewall_policy_id = var.isWAF? azurerm_web_application_firewall_policy.network[0].id : null
 
 #고정값
   sku {
-    name     = "WAF_v2"
-    tier     = "WAF_v2"
+    name     = var.isWAF? "WAF_v2" : "Standard_v2"
+    tier     = var.isWAF? "WAF_v2" : "Standard_v2"
     capacity = 1
   }
 
@@ -79,21 +64,21 @@ resource "azurerm_application_gateway" "network" {
   }
 
   frontend_port {
-    name = local.frontend_port_name
-    port = var.appgw_config.frontend_port
+    name = var.appgw_config.frontend_port.name
+    port = var.appgw_config.frontend_port.port
   }
 
   frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
+    name                 = var.appgw_config.frontend_ip_configuration
     public_ip_address_id = azurerm_public_ip.example.id
   }
 
   backend_address_pool {
-    name = local.backend_address_pool_name
+    name = var.appgw_config.backend_address_pool
   }
 
   backend_http_settings {
-    name                  = local.http_setting_name
+    name                  = var.appgw_config.backend_http_settings.name
     cookie_based_affinity = var.appgw_config.backend_http_settings.cookie_based_affinity
     path                  = var.appgw_config.backend_http_settings.path
     port                  = var.appgw_config.backend_http_settings.port
@@ -102,18 +87,18 @@ resource "azurerm_application_gateway" "network" {
   }
 
   http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = var.appgw_config.http_listener
+    name                           = var.appgw_config.http_listener.name
+    frontend_ip_configuration_name = var.appgw_config.frontend_ip_configuration
+    frontend_port_name             = var.appgw_config.frontend_port.name
+    protocol                       = var.appgw_config.http_listener.protocol
   }
 
   request_routing_rule {
-    name                       = local.request_routing_rule_name
+    name                       = var.appgw_config.request_routing_rule.name
     rule_type                  = var.appgw_config.request_routing_rule.rule_type
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
+    http_listener_name         = var.appgw_config.http_listener.name
+    backend_address_pool_name  = var.appgw_config.backend_address_pool
+    backend_http_settings_name = var.appgw_config.backend_http_settings.name
     priority                   = var.appgw_config.request_routing_rule.priority
   }
 }
